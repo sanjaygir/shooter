3f28bdc1c24a21e081319abc81b7ce03d6fdc069
sanjaygir <sanjaygir@gmail.com>
2014-06-03 19:33:59 -0600

    desktop launcher updated package

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -1,8 +1,9 @@
 package com.mygdx.game.desktop;
 
+import game.Game;
+
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.Game;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
